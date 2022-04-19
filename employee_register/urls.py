2dd3cba202c90357c06bc6ca1369691b500ccb12
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-04-19 03:41:13 +0200

    update

---
+++ employee_register/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.employee_form),
+    path('<int:id>/', views.employee_form, name = 'employee_update'),
+    path('list/', views.employee_list)
+]
